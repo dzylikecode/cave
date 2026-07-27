f6ef551bab595969f67a7fbb68ebcf72d80e7571
Dz <MathBeWithCode@gmail.com>
2026-07-28 01:27:59 +0800

    sync

--- packages/py_embed/lib/src/venv.dart
+++ packages/py_embed/lib/src/venv.dart
@@ -15,5 +15,29 @@ Future<String> runPyShell(String code, [String pyExe = 'python']) async {
   return result.stdout.toString().trim();
 }
 
-Future<String> getPyPrefixFromShell([String pyExe = 'python']) => runPyShell('import sys; print(sys.prefix)', pyExe);
-Future<String> getPyExecutableFromShell([String pyExe = 'python']) => runPyShell('import sys; print(sys.executable)', pyExe);
+String runPyShellSync(String code, [String pyExe = 'python']) {
+  final result = Process.runSync(pyExe, ['-c', code]);
+
+  if (result.exitCode != 0) {
+    throw ProcessException(
+      pyExe,
+      ['-c', code],
+      result.stderr.toString(),
+      result.exitCode,
+    );
+  }
+
+  return result.stdout.toString().trim();
+}
+
+Future<String> getPyPrefixFromShell([String pyExe = 'python']) =>
+    runPyShell('import sys; print(sys.prefix)', pyExe);
+Future<String> getPyExecutableFromShell([String pyExe = 'python']) =>
+    runPyShell('import sys; print(sys.executable)', pyExe);
+
+String getPyPrefixFromShellSync([String pyExe = 'python']) =>
+    runPyShellSync('import sys; print(sys.prefix)', pyExe);
+
+String getPyExecutableFromShellSync([String pyExe = 'python']) =>
+    runPyShellSync('import sys; print(sys.executable)', pyExe);
+
