78e772d42b58fcb9044a8f96155eb3c3bad1dbb2
Dz <MathBeWithCode@gmail.com>
2026-07-31 14:36:24 +0800

    onnx_runtime

---
+++ packages/onnx_runtime/example/onnx_runtime_example.dart
@@ -0,0 +1,6 @@
+import 'package:onnx_runtime/onnx_runtime.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/onnx_runtime/test/onnx_runtime_test.dart
@@ -0,0 +1,16 @@
+import 'package:onnx_runtime/onnx_runtime.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
